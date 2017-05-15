14336eab1a0a181eb34ba30995714ba3ce186bb7
aboveyou00 <brandonyoyoslade@gmail.com>
2017-05-15 16:39:23 -0600

    test(math): add unit tests for the math utility helpers

---
+++ ts-webpack-spike/ts-webpack-spike/src/utils/test/math.spec.ts
@@ -0,0 +1,26 @@
+﻿/// <reference types="mocha" />
+
+import { expect, use } from 'chai';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+use(sinonChai);
+
+import { degToRad, radToDeg } from '../math';
+
+describe('utils/degToRad', () => {
+    it('converts 0 to 0', () => {
+        expect(degToRad(0)).to.eql(0);
+    });
+    it('converts 180 to 3.14159', () => {
+        expect(degToRad(180)).to.eql(Math.PI);
+    });
+});
+
+describe('utils/radToDeg', () => {
+    it('converts 0 to 0', () => {
+        expect(radToDeg(0)).to.eql(0);
+    });
+    it('converts 3.14159 to 180', () => {
+        expect(radToDeg(Math.PI)).to.eql(180);
+    });
+});
