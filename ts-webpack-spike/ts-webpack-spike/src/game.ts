e95b56274af83e853269355ca44345ec8172a42d
aboveyou00 <brandonyoyoslade@gmail.com>
2017-05-15 15:14:41 -0600

    feat(loop): add simple GameObject and TestObject; implement tick and render

--- ts-webpack-spike/ts-webpack-spike/src/game.ts
+++ ts-webpack-spike/ts-webpack-spike/src/game.ts
@@ -1,4 +1,5 @@
-﻿
+﻿import { GameObject } from './game-object';
+import { TestObject } from './test-object';
 
 export class Game {
     constructor(private framesPerSecond = 30) { }
@@ -10,7 +11,16 @@ export class Game {
     start() {
         this.canvas = <HTMLCanvasElement>document.getElementById('gameCanvas');
         this.context = this.canvas.getContext("2d");
+        this.canvas.width = 640;
+        this.canvas.height = 480;
         setInterval(() => this.onTick(), 1000 / this.framesPerSecond);
+
+        this.addObject(new TestObject());
+    }
+
+    private _objects: GameObject[] = [];
+    addObject(obj: GameObject) {
+        this._objects.push(obj);
     }
     
     private onTick() {
@@ -18,8 +28,18 @@ export class Game {
         let delta = (this.previousTick == null) ? 0 : (currentTime.valueOf() - this.previousTick.valueOf()) / 1000;
         this.previousTick = currentTime;
         this.tick(delta);
+        this.render(this.context);
     }
     private tick(delta: number) {
-
+        for (let obj of this._objects) {
+            obj.tick(delta);
+        }
+    }
+    private render(context: CanvasRenderingContext2D) {
+        context.fillStyle = 'pink';
+        context.fillRect(0, 0, 100, 100);
+        for (let obj of this._objects) {
+            if (obj.shouldRender) obj.render(context);
+        }
     }
 }
