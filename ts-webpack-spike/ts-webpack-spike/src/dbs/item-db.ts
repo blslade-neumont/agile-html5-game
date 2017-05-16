d392bd11c2bb55a9e50519d9ebd0118ff509f80d
Sen <keima.ayasaki@gmail.com>
2017-05-16 16:02:54 -0600

    +Added nine items to item-db.ts +Added three tiles to tile-db.ts

--- ts-webpack-spike/ts-webpack-spike/src/dbs/item-db.ts
+++ ts-webpack-spike/ts-webpack-spike/src/dbs/item-db.ts
@@ -13,6 +13,62 @@ export let items: GameItem[] = [
         sprite: {
             src: 'images/IconSet.png',
             tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 8 }
-        }
-    }
+        },
+    },
+    {
+        name: 'Plant_2',
+        sprite: {
+            src: 'images/IconSet.png',
+            tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 9 }
+        },
+    },
+    {
+        name: 'Plant_3',
+        sprite: {
+            src: 'images/IconSet.png',
+            tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 10 }
+        },
+    },
+    {
+        name: 'Plant_4',
+        sprite: {
+            src: 'images/IconSet.png',
+            tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 11 }
+        },
+    },
+    {
+        name: 'Plant_5',
+        sprite: {
+            src: 'images/IconSet.png',
+            tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 12 }
+        },
+    },
+    {
+        name: 'Plant_6',
+        sprite: {
+            src: 'images/IconSet.png',
+            tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 13 }
+        },
+    },
+    {
+        name: 'Plant_7',
+        sprite: {
+            src: 'images/IconSet.png',
+            tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 14 }
+        },
+    },
+    {
+        name: 'Plant_8',
+        sprite: {
+            src: 'images/IconSet.png',
+            tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 15 }
+        },
+    },
+    {
+        name: 'Carrot',
+        sprite: {
+            src: 'images/IconSet.png',
+            tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 18, tiley: 0 }
+        },
+    },
 ];
