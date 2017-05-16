6958f6dfa65700e8d8fb4072f2f874a614295837
Sen <keima.ayasaki@gmail.com>
2017-05-16 16:51:48 -0600

    Adding Unit Test +Added test/render.spec.ts

---
+++ ts-webpack-spike/ts-webpack-spike/src/utils/test/render.spec.ts
@@ -0,0 +1,18 @@
+﻿/// <reference types="mocha" />
+
+import { expect, use } from 'chai';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+use(sinonChai);
+
+import { fillText } from '../render';
+import { stubCanvas } from '../../test/mock-canvas';
+
+describe('utils/fillText', () => {
+    stubCanvas();
+
+    it('should split strings on new lines and render to context', () => {
+        expect(() => fillText(new HTMLCanvasElement().getContext("2d"), "和書委\n和書委\n和書委\n和書委", 0, 0)).not.to.throw();
+    });
+});
+
