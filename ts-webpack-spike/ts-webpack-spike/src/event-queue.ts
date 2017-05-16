66c888c748cd1f67595ac40630d9315433d1c874
aboveyou00 <brandonyoyoslade@gmail.com>
2017-05-16 16:13:52 -0600

    feat(events): add mouse events; UNTESTED

--- ts-webpack-spike/ts-webpack-spike/src/event-queue.ts
+++ ts-webpack-spike/ts-webpack-spike/src/event-queue.ts
@@ -1,6 +1,7 @@
-﻿import { GameEvent, KeyTypedEvent, KeyPressedEvent, KeyReleasedEvent } from './utils/events';
+﻿import { GameEvent, MouseButton } from './utils/events';
 
 const DEBUG_KEYS = false;
+const DEBUG_MOUSE = true;
 
 export class EventQueue {
     constructor() {
@@ -8,6 +9,10 @@ export class EventQueue {
     }
     private init() {
         let body = document.getElementsByTagName('body')[0];
+        this.initKeyboard(body);
+        this.initMouse(body);
+    }
+    private initKeyboard(body: HTMLBodyElement) {
         body.onkeydown = e => {
             if (DEBUG_KEYS) console.log(`Pressed: ${e.key}; ${e.code}`);
             if (!this.isKeyDown(e.code)) {
@@ -43,13 +48,68 @@ export class EventQueue {
             }
         };
     }
+    private initMouse(body: HTMLBodyElement) {
+        body.onmousemove = e => {
+            if (DEBUG_MOUSE) console.log(`Mouse moved. Movement: ${e.movementX}, ${e.movementY}; Position: ${e.pageX}, ${e.pageY}`);
+            this.enqueue({
+                type: 'mouseMoved',
+                movementX: e.movementX,
+                movementY: e.movementY,
+                pageX: this._pageX = (e.pageX || (this._pageX + e.movementX)),
+                pageY: this._pageY = (e.pageY || (this._pageY + e.movementY))
+            });
+        };
+        body.onmousedown = e => {
+            if (DEBUG_MOUSE) console.log(`Mouse button pressed. Button: ${e.button}; Position: ${e.pageX}, ${e.pageY}`);
+            if (!this.isMouseButtonDown(e.button)) {
+                this.enqueue({
+                    type: 'mouseButtonPressed',
+                    button: <MouseButton>e.button,
+                    pageX: this._pageX = e.pageX || this._pageX,
+                    pageY: this._pageY = e.pageY || this._pageY
+                });
+                this._mouseButtons.set(e.button, true);
+            }
+        };
+        body.onmouseup = e => {
+            if (DEBUG_MOUSE) console.log(`Mouse button released. Button: ${e.button}; Position: ${e.pageX}, ${e.pageY}`);
+            if (this.isMouseButtonDown(e.button)) {
+                this.enqueue({
+                    type: 'mouseButtonReleased',
+                    button: <MouseButton>e.button,
+                    pageX: this._pageX = e.pageX || this._pageX,
+                    pageY: this._pageY = e.pageY || this._pageY
+                });
+                this._mouseButtons.set(e.button, true);
+            }
+        };
+        body.onmousewheel = e => {
+            if (DEBUG_MOUSE) console.log(`Mouse wheel. delta: ${e.wheelDelta}; Position: ${e.pageX}, ${e.pageY}`);
+            this.enqueue({
+                type: 'mouseWheel',
+                delta: e.wheelDelta,
+                pageX: this._pageX = e.pageX || this._pageX,
+                pageY: this._pageY = e.pageY || this._pageY
+            });
+        };
+    }
 
     private _events: GameEvent[] = [];
     private _keys = new Map<string, boolean>();
+    private _mouseButtons = new Map<MouseButton, boolean>();
+    private _pageX: number = 0;
+    private _pageY: number = 0;
 
     isKeyDown(code: string) {
         if (!this._keys.has(code)) return false;
         return this._keys.get(code);
+    }
+    isMouseButtonDown(button: number) {
+        if (!this._mouseButtons.has(button)) return false;
+        return this._mouseButtons.get(button);
+    }
+    get mousePosition() {
+        return { x: this._pageX, y: this._pageY };
     }
 
     enqueue(e: GameEvent) {
