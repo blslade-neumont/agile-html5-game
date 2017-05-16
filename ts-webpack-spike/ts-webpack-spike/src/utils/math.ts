2fba7e1da4fa091e20d336fb8f6c4b88165fd649
aboveyou00 <brandonyoyoslade@gmail.com>
2017-05-15 20:45:14 -0600

    test(game-object): add unit tests for GameObject and fixed a few movement bugs

--- ts-webpack-spike/ts-webpack-spike/src/utils/math.ts
+++ ts-webpack-spike/ts-webpack-spike/src/utils/math.ts
@@ -6,3 +6,10 @@ export function degToRad(deg: number) {
 export function radToDeg(rad: number) {
     return (rad / Math.PI) * 180;
 }
+
+export function fmod(a: number, b: number) {
+    //Adapted from https://gist.github.com/wteuber/6241786
+    //TODO: this implementation loses precision. Doesn't matter much in games, but still
+    //Besides that, it also converts to a string and back. Yikes.
+    return +(a - (Math.floor(a / b) * b)).toPrecision(8);
+}
