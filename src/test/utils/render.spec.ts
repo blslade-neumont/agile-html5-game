bc8b9b5feb2cdc0d7fa42ea16074f1d0ea08d894
aboveyou00 <brandonyoyoslade@gmail.com>
2017-05-17 14:48:21 -0600

    test(utils): add more unit tests for fillText

--- src/test/utils/render.spec.ts
+++ src/test/utils/render.spec.ts
@@ -11,8 +11,23 @@ import { stubCanvas } from '../mock-canvas';
 describe('utils/fillText', () => {
     stubCanvas();
 
+    let context: CanvasRenderingContext2D;
+    beforeEach(() => {
+        context = new HTMLCanvasElement().getContext("2d");
+    });
+
     it('should split strings on new lines and render to context', () => {
-        expect(() => fillText(new HTMLCanvasElement().getContext("2d"), "和書委\n和書委\n和書委\n和書委", 0, 0)).not.to.throw();
+        expect(() => fillText(context, "和書委\n和書委\n和書委\n和書委", 0, 0)).not.to.throw;
+    });
+
+    it('should invoke context.renderText for every line of text', () => {
+        sinon.stub(context, 'fillText');
+        fillText(context, 'a\nb\nHello\nWorld', 0, 0);
+        let subject = expect(context.fillText).to.have.been;
+        subject.callCount(4);
+        subject.calledWith('a');
+        subject.calledWith('b');
+        subject.calledWith('Hello');
+        subject.calledWith('World');
     });
 });
-
