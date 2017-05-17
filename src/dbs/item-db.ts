901ab32e213e972dccd6a60b6cfe1ed64606038d
Sen <keima.ayasaki@gmail.com>
2017-05-17 16:45:01 -0600

    Added a database and moved assets +Changed location of IconSet.png, Outside_A2.png, and Outside_Tiles.png to be located in respective folders inside of images +Forced katie back into game dev. +Created a database of alive-db.ts for things that can die.

--- src/dbs/item-db.ts
+++ src/dbs/item-db.ts
@@ -11,63 +11,63 @@ export let items: { [name: string]: GameItem } =
         plant_1:
         {
             sprite: {
-                src: 'images/IconSet.png',
+                src: 'images/Icons/IconSet.png',
                 tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 8 }
             },
         },
         plant_2:
         {
             sprite: {
-                src: 'images/IconSet.png',
+                src: 'images/Icons/IconSet.png',
                 tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 9 }
             },
         },
         plant_3:
         {
             sprite: {
-                src: 'images/IconSet.png',
+                src: 'images/Icons/IconSet.png',
                 tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 10 }
             },
         },
         plant_4:
         {
             sprite: {
-                src: 'images/IconSet.png',
+                src: 'images/Icons/IconSet.png',
                 tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 11 }
             },
         },
         plant_5:
         {
             sprite: {
-                src: 'images/IconSet.png',
+                src: 'images/Icons/IconSet.png',
                 tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 12 }
             },
         },
         plant_6:
         {
             sprite: {
-                src: 'images/IconSet.png',
+                src: 'images/Icons/IconSet.png',
                 tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 13 }
             },
         },
         plant_7:
         {
             sprite: {
-                src: 'images/IconSet.png',
+                src: 'images/Icons/IconSet.png',
                 tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 14 }
             },
         },
         plant_8:
         {
             sprite: {
-                src: 'images/IconSet.png',
+                src: 'images/Icons/IconSet.png',
                 tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 12, tiley: 15 }
             },
         },
         crop_carrot:
         {
             sprite: {
-                src: 'images/IconSet.png',
+                src: 'images/Icons/IconSet.png',
                 tileset: { width: ITEM_SIZE, height: ITEM_SIZE, tilex: 18, tiley: 0 }
             },
         }
