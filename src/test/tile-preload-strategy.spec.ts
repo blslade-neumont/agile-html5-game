cb73f149c2014f2bd1b2f4546f80d83467062497
Justin Furtado <justin.l.furtado@gmail.com>
2017-05-18 13:28:19 -0600

    Add unit tests for loading strategies

---
+++ src/test/tile-preload-strategy.spec.ts
@@ -0,0 +1,37 @@
+﻿/// <reference types="mocha" />
+
+import { expect, use } from 'chai';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+use(sinonChai);
+
+import { ResourceLoader, PreloadStrategy } from '../engine';
+import { tiles } from '../dbs/tile-db';
+import { TilePreloadStrategy } from '../tile-preload-strategy';
+
+import { AgileGame } from '../agile-game';
+import { GameObject, delay } from '../engine';
+import { stubDocument, stubImage, stubCanvas } from '../engine/test';
+
+describe('TilePreloadStrategy', () => {
+    stubDocument();
+    stubImage();
+    stubCanvas();
+
+    let game: AgileGame;
+    beforeEach(() => {
+        game = new AgileGame(30, new HTMLCanvasElement());
+    });
+    afterEach(() => {
+        if (game.isRunning) game.stop();
+    });
+
+    it('should call loadImage once per tile in the database', () => {
+        let preloadStrategy: TilePreloadStrategy = new TilePreloadStrategy();
+
+        sinon.stub(game.resourceLoader, 'loadImage');
+
+        preloadStrategy.preload(game.resourceLoader);
+        expect(game.resourceLoader.loadImage).callCount(Object.keys(tiles).length);
+    });
+});
