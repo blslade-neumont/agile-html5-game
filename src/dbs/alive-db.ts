592a8b63af448dadb77e4e264970fb9e73138132
aboveyou00 <brandonyoyoslade@gmail.com>
2017-05-18 20:33:42 -0600

    feat(player): add animated sprites to the player

--- src/dbs/alive-db.ts
+++ src/dbs/alive-db.ts
@@ -9,7 +9,49 @@ export let alives: { [name: string]: Alive } =
         katie_south: {
             sprite: {
                 src: 'images/Alive/Katie.png',
-                tileset: { width: 32, height: 32, tilex: 0, tiley: 0 }
+                tileset: { width: 32, height: 32 },
+                frames: [
+                    { tilex: 0, tiley: 0 },
+                    { tilex: 1, tiley: 0 },
+                    { tilex: 2, tiley: 0 },
+                    { tilex: 1, tiley: 0 }
+                ]
+            }
+        },
+        katie_west: {
+            sprite: {
+                src: 'images/Alive/Katie.png',
+                tileset: { width: 32, height: 32 },
+                frames: [
+                    { tilex: 0, tiley: 1 },
+                    { tilex: 1, tiley: 1 },
+                    { tilex: 2, tiley: 1 },
+                    { tilex: 1, tiley: 1 }
+                ]
+            }
+        },
+        katie_east: {
+            sprite: {
+                src: 'images/Alive/Katie.png',
+                tileset: { width: 32, height: 32 },
+                frames: [
+                    { tilex: 0, tiley: 2 },
+                    { tilex: 1, tiley: 2 },
+                    { tilex: 2, tiley: 2 },
+                    { tilex: 1, tiley: 2 }
+                ]
+            }
+        },
+        katie_north: {
+            sprite: {
+                src: 'images/Alive/Katie.png',
+                tileset: { width: 32, height: 32 },
+                frames: [
+                    { tilex: 0, tiley: 3 },
+                    { tilex: 1, tiley: 3 },
+                    { tilex: 2, tiley: 3 },
+                    { tilex: 1, tiley: 3 }
+                ]
             }
         }
     };
