835e51660f62dbb219ea6d321edcced21e82a646
aboveyou00 <brandonyoyoslade@gmail.com>
2017-05-31 16:36:24 -0600

    test(agile-game): add unit tests for GameOverScene

--- src/scenes/test/game-over-scene.spec.ts
+++ src/scenes/test/game-over-scene.spec.ts
@@ -5,16 +5,53 @@ import * as sinon from 'sinon';
 import * as sinonChai from 'sinon-chai';
 use(sinonChai);
 
+import { GameOverScene } from '../game-over-scene';
 import { TitleScene } from '../title-scene';
-import { Game } from '../../engine';
+import { Game, AudioSourceObject } from '../../engine';
+import { MockGame } from '../../engine/test';
 
-describe('TitleScene', () => {
+describe('GameOverScene', () => {
     let game: Game;
-    let scene: TitleScene;
+    let scene: GameOverScene;
     beforeEach(() => {
-        scene = new TitleScene();
-        scene.game = game = <any>{ scene: scene };
+        scene = new GameOverScene();
+        game = scene.game = <any>new MockGame(scene);
     });
 
-    
+    describe('.start', () => {
+        it('should create an AudioSourceObject for the game over music', () => {
+            sinon.stub(scene, 'addObject');
+            scene.start();
+            expect(scene.addObject).to.have.been.calledWith(sinon.match(obj => obj instanceof AudioSourceObject && obj.name.match(/music/i)));
+        });
+    });
+
+    describe('.handleEvent', () => {
+        beforeEach(() => {
+            scene.start();
+        });
+
+        it('should navigate to the title scene when enter is pressed', () => {
+            sinon.stub(game, 'changeScene');
+            scene.handleEvent({
+                type: 'keyPressed',
+                code: 'Enter',
+                shiftPressed: false,
+                ctrlPressed: false,
+                altPressed: false
+            });
+            expect(game.changeScene).to.have.been.calledWith(sinon.match.instanceOf(TitleScene));
+        });
+        it('should navigate to the title scene when escape is pressed', () => {
+            sinon.stub(game, 'changeScene');
+            scene.handleEvent({
+                type: 'keyPressed',
+                code: 'Escape',
+                shiftPressed: false,
+                ctrlPressed: false,
+                altPressed: false
+            });
+            expect(game.changeScene).to.have.been.calledWith(sinon.match.instanceOf(TitleScene));
+        });
+    });
 });
